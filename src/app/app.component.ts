97a084579643001d7f241f31123fcf1209a483ef
Cavid Haciyev <javid.hajiyev@ziraatbank.az>
2023-02-14 17:58:34 +0400

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import * as $ from 'jquery'
+import {async, elementAt} from "rxjs";
+
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: '<div><h1>Hello</h1><br>  <button (click)="sendRequest()" > Click me </button></div>'
+  // templateUrl: './app.component.html',
+  // styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'JQueryInAngular';
+  url: string="https://jsonplaceholder.typicode.com/todos/";
+
+  sendRequest() {
+    fetch(this.url)
+     .then(response => response.text())
+     .then(result => console.log(result))
+     .catch(error => console.log('error', error));
+  }
+
+  ngOnInit() {
+    // $(`button`).click(function () {
+    //   alert(`Clicked!`);
+    // });
+
+    $(`div`).css(`background-color`, `rgba(42, 149, 189, 0.6)`)
+
+  }
 }
+
